6228c4f0b7c36e2f631813af32cb4fcdbf7efe60
tkznzl12 <74891586+tkznzl12@users.noreply.github.com>
2025-08-19 12:26:42 +0900

    에러핸들링

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -17,8 +17,8 @@ import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 async function AllBooks() {
   await delay(2000);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
-    { cache: "force-cache" }
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+    // { cache: "force-cache" }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
@@ -38,8 +38,8 @@ async function AllBooks() {
 async function RecoBooks() {
   await delay(1500);
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
-    { next: { revalidate: 3 } }
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    // { next: { revalidate: 3 } }
   );
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
---
+++ src/app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error(error.message);
+  }, [error]);
+
+  return (
+    <div>
+      <h3>검색 과정에서 오류가 발생했습니다.</h3>
+      <button
+        onClick={() => {
+          //일괄적으로 함수를 동기적으로 처리 가능
+          startTransition(() => {
+            router.refresh(); // 현재 페이지에 필요한 서버컴포넌트들을 다시 불러옴
+            reset();
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </div>
+  );
+}
